67b1cd077df0a2d85797ebe993e1d369712ecdb5
Tasnim Islam <tasnim.nyc@gmail.com>
2024-09-30 09:55:09 -0400

    debug

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,14 +1,13 @@
 public class MyArrays {
-    public static String arrayToString(int[][]nums){
+    public static String arrayToString(int[] nums){
         String s = "[";
-        for (int i = 0; i < nums.length; i++ ){
-            if (i < nums.length - 1){
-                s += arrayToString(nums[i]);
+        for (int i = 0; i < nums.length - 1; i++ ){
+                s += nums[i];
                 s += ", ";
-            }
+            
         }
-        s += arrayToString(nums[nums.length - 1]) + "]";
-        return s;
+        s += nums[nums.length - 1] ;
+        return s + "}";
 
     }
 
@@ -44,27 +43,35 @@ public class MyArrays {
         int[] copy = returnCopy(testArrays[i]);
         System.out.println("Original: " + arrayToString(testArrays[i]) + " | Copy: " + arrayToString(copy));
     }
-  
+    int[][] array1Tests = {
+        {1, 2, 3},                  
+        {5},            
+        {7, 8, 9, 10},  
+        {0, -1}          
+    };
 
-      int[][] array1Tests = {
-          {1, 2, 3},                  
-          {5},             
-          {7, 8, 9, 10},   
-          {0, -1}          
-      };
 
-      int[][] array2Tests = {
-          {4, 5, 6},             
-          {2},             
-          {11, 12},       
-          {3, 4}          
-      };
+    int[][] array2Tests = {
+        {4, 5, 6},            
+        {2},            
+        {11, 12},      
+        {3, 4}          
+    };
 
-      for (int i = 0; i < array1Tests.length; i++) {
-          int[] concatenated = concatArray(array1Tests[i], array2Tests[i]);
-          System.out.println("Array1: " + arrayToString(array1Tests[i]) + 
-                             " | Array2: " + arrayToString(array2Tests[i]) + 
-                             " | Concatenated: " + arrayToString(concatenated));
-      }
+
+ 
+
+
+
+
+for (int i = 0; i < array1Tests.length; i++) {
+        int[] concatenated = concatArray(array1Tests[i], array2Tests[i]);
+        System.out.println("Array1: " + arrayToString(array1Tests[i]) +
+                           " | Array2: " + arrayToString(array2Tests[i]) +
+                           " | Concatenated: " + arrayToString(concatenated));
+    }
+
+
+
   }
 }
