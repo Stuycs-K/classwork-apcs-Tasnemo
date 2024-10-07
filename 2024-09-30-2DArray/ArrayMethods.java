4c97ac8f2446b9ed71958589c82f61e528144173
Tasnim Islam <tasnim.nyc@gmail.com>
2024-10-07 00:21:40 -0400

    made 3 & 4

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -10,7 +10,11 @@ public class ArrayMethods {
     test = new int[][] {{1,3,5}, {1,2},{}};
     System.out.println(arr2DSum(test));
   
-
+    test = new int[][] {{1,2,-3}, {4,5,6},{7,8,9}};
+    System.out.println(arrToString(swapRC(test)));
+    test = new int[][] {{1,3,5}, {1,2,4},{5,6,7}};
+    System.out.println(arrToString(swapRC(test)));
+  
   
   }
 
@@ -59,8 +63,8 @@ public static String arrToString(int[][]ary){
 public static int arr2DSum(int[][]nums){
   int sum = 0;
   for (int i = 0; i < nums.length; i++) {
-    for (int n = 0; n nums[i].length; n++) {
-      sum += nums[i][n]
+    for (int n = 0; n <nums[i].length; n++) {
+      sum += nums[i][n];
     }
   }
 
@@ -72,7 +76,53 @@ public static int arr2DSum(int[][]nums){
   * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
   */
 public static int[][] swapRC(int[][]nums){
-  return new int[1][1];
+  int[][] copy= new int[nums[0].length][nums.length];
+  for(int i = 0; i < nums[0].length; i++) {
+    for (int n = 0; n < nums.length; n++) {
+      copy[i][n] = nums[n][i];
+    }
+  }
+  return copy;
 }
 
+//3. Modify a given 2D array of integer as follows:
+//Replace all the negative values:
+//-When the row number is the same as the column number replace
+//that negative with the value 1
+//-All other negatives replace with 0
+public static void replaceNegative(int[][] vals){ 
+  for(int i = 0; i < vals[0].length; i++) {
+    for (int n = 0; n < vals.length; n++) {
+      if (vals[n][i] < 0) {
+        if (n == i) {
+          vals[n][i] = 1;
+        } else {
+          vals[n][i] = 0;
+        }
+      }
+    }
+  }
 }
+
+//4. Make a copy of the given 2d array.
+//When testing : make sure that changing the original does NOT change the copy.
+//DO NOT use any built in methods that "copy" an array.
+//You SHOULD write a helper method for this.
+//If you don't see a good way to do that, you should stop and look at prior methods.
+public static int[] copy(int[] nums) {
+  int[] copy = new int[nums.length];
+  for (int i = 0; i < nums.length; i++) {
+    copy[i] = nums[i];
+  }
+  return copy;
+}
+public static int[][] copy(int[][] nums){
+  int[][] copy = new int[nums.length][nums[0].length];
+  for (int i = 0; i < nums.length; i++) {
+    copy[i] = copy(nums[i]);
+
+  }
+  return copy;
+}
+}
+
