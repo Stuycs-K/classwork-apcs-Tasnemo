75e7cb5cb8a9fe66a48f8fda899df2ff841e91b3
Tasnim Islam <tasnim.nyc@gmail.com>
2024-11-14 11:42:31 -0500

    testing the replaceempty method

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -9,9 +9,26 @@ public class ArrayListPractice {
       } else {
         RanArry.add("" +num);
       }
-    p
     }
     return RanArry;
   }
+  public static void replaceEmpty( ArrayList<String> original){
+  //Modify the ArrayList such that it has all of the empty strings are
+  //replaced with the word "Empty".
+  for (int i = 0; i < original.size();i++){
+    if (original.get(i)==""){
+      original.set(i,"Empty");
+    }
+  }
+}
 
+public static ArrayList<String> makeReversedList( ArrayList<String> original){
+  //return a new ArrayList that is in the reversed order of the original.
+}
+
+public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
+  //return a new ArrayList that has all values of a and b in alternating order that is:
+  //a[0], b[0], a[1], b[1]...
+  //If one list is longer than the other, just attach the remaining values to the end.
+}
 }
