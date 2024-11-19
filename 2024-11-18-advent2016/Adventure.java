fbffaa57ed4f5b0504c09474b2bb832c15035bc4
Tasnim Islam <tasnim.nyc@gmail.com>
2024-11-19 11:45:40 -0500

    start the travel method finished the parsers

--- 2024-11-18-advent2016/Adventure.java
+++ 2024-11-18-advent2016/Adventure.java
@@ -2,91 +2,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 
 public class Adventure {
 
-    private ArrayList<String> instructions;
-    private int x, y;
-    private String currentDirection;
+ public static String[] parse(String filename) {
+   try {
+       File file = new File(filename);
+       Scanner input = new Scanner(file);
+       String line = input.nextLine();
+      input.close();
+       String[] dirs = line.split(", ");
 
-    public Adventure(ArrayList<String> instructions) {
-        this.instructions = instructions;
-        this.x = 0;
-        this.y = 0;
-        this.currentDirection = "N"; // Initially facing North
-    }
 
-    public void turnLeft() {
-        // Turn left from the current direction
-        if (currentDirection.equals("N")) {
-            currentDirection = "W";
-        } else if (currentDirection.equals("W")) {
-            currentDirection = "S";
-        } else if (currentDirection.equals("S")) {
-            currentDirection = "E";
-        } else if (currentDirection.equals("E")) {
-            currentDirection = "N";
-        }
-    }
 
-    public void turnRight() {
-        // Turn right from the current direction
-        if (currentDirection.equals("N")) {
-            currentDirection = "E";
-        } else if (currentDirection.equals("E")) {
-            currentDirection = "S";
-        } else if (currentDirection.equals("S")) {
-            currentDirection = "W";
-        } else if (currentDirection.equals("W")) {
-            currentDirection = "N";
-        }
-    }
+     }
+   catch (FileNotFoundException ex) {
+       System.out.println("File not found");
+   }
+   return null;
+ }
 
-    public void moveForward(int steps) {
-        // Move forward the specified number of steps in the current direction
-        if (currentDirection.equals("N")) {
-            y += steps;
-        } else if (currentDirection.equals("E")) {
-            x += steps;
-        } else if (currentDirection.equals("S")) {
-            y -= steps;
-        } else if (currentDirection.equals("W")) {
-            x -= steps;
-        }
-    }
+ public static int travel(String[] instruc) {
+   int x = 0;
+   int y = 0;
+   int O = 0;
+   int[][] oreo =  {
+   {0,1}, {1,0}, {0,-1}, {-1,0}};
+   int[] travels = new int[instruc.length()/2];
+   String[] turns = new String[instruc.length(/2)];
+   for( int i = 0; i < instruc.length(); i++) {}
 
-    public int run() {
-        // Process each instruction in the sequence
-        for (String instruction : instructions) {
-            char turn = instruction.charAt(0);  // First character (L or R)
-            int steps = Integer.parseInt(instruction.substring(1));  // Remaining part is the number of steps
+   return 0;
+ }
 
-            if (turn == 'L') {
-                turnLeft();
-            } else if (turn == 'R') {
-                turnRight();
-            }
-
-            moveForward(steps);
-        }
-
-        // Return the Manhattan distance
-        return Math.abs(x) + Math.abs(y);
-    }
-
-    public static ArrayList<String> readInstructionsFromFile(String filename) {
-        ArrayList<String> instructions = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File(filename))) {
-            // Read the single line from the file
-            if (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                // Instructions are expected to be comma-separated
-                instructions = Array.asList(line.split(", "));
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return instructions;
-    }
 }
--- 2024-11-18-advent2016/Main.java
+++ 2024-11-18-advent2016/Main.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Read instructions from input.txt using Scanner
-        List<String> instructions = Adventure.readInstructionsFromFile("input.txt");
-
-        // Create an instance of the Adventure class with the instructions
-        Adventure adventure = new Adventure(instructions);
-
-        // Run the adventure and output the result
-        int distance = adventure.run();
-        System.out.println("The shortest distance to the destination is " + distance + " blocks.");
+      Adventure.parse("input.txt");
     }
 }
