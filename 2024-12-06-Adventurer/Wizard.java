0d3a6977a5d0f2d5cfeae024ba11b8491829fe4e
Tasnemo <tasnim.nyc@gmail.com>
2024-12-08 20:37:36 -0500

    make constructors

--- 2024-12-06-Adventurer/Wizard.java
+++ 2024-12-06-Adventurer/Wizard.java
@@ -1,35 +1,50 @@
 import java.util.Random;
+
 public class Wizard extends Adventurer {
   private String special = "mana";
   private int specialMax = 10;
   private int specialQ = 10;
 
+  public Wizard() {
+    super("name", 10);
+  }
+
   public String getSpecialName() {
     return special;
   }
+
   public int getSpecial() {
-      return specialQ;
-    }
-    public int getSpecialMax() {
-      return specialMax;
-    }
-  public void setSpecial(int n) {
-    if( n <= getSpecialMax()) {
-    specialQ = n;
+    return specialQ;
   }
-}
-public String attack(Adventurer other) {
-  other.setHP(other.getHP() - 1);
-  return "attac";
-}
 
-//heall or buff the target adventurer
-public String support(Adventurer other);
+  public int getSpecialMax() {
+    return specialMax;
+  }
 
-//heall or buff self
-public String support();
+  public void setSpecial(int n) {
+    if (n <= getSpecialMax()) {
+      specialQ = n;
+    }
+  }
 
-//hurt or hinder the target adventurer, consume some special resource
-public String specialAttack(Adventurer other);
+  public String attack(Adventurer other) {
+    other.setHP(other.getHP() - 1);
+    return "attac";
+  }
+
+  // heall or buff the target adventurer
+  public String support(Adventurer other) {
+    return null;
+  }
+
+  // heall or buff self
+  public String support() {
+    return null;
+  }
+
+  // hurt or hinder the target adventurer, consume some special resource
+  public String specialAttack(Adventurer other) {
+    return null;
+  }
 
 }
