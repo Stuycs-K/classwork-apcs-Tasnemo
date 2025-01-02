dac05f2fe2bcff4acce5b5bab6ac22cd7dcd5ae8
Tasnim Islam <tasnim.nyc@gmail.com>
2025-01-02 11:42:39 -0500

    doing classwork

---
+++ 2025-01-02/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+  public static void main (String[] args) {
+    System.out.println(fib(Integer.parseInt(args[0])));
+  }
+  public static int fib( int ind) {
+    if (ind == 0)  return 0;
+    if (ind == 1)  return 1;
+    return fib(ind-1) + fib(ind-2);
+  }
+}
